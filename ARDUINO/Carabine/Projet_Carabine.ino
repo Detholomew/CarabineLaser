caadc2e52fb1b71085f214be34ebb9014b8ba140
IESE4Biathlon <IESE42017@gmail.com>
2017-04-27 11:08:48 +0200

    Version finale (ou presque de la carabine)
    Implémentation des affichages sur le LCD etc.. Manque uniquement le XBee

--- ARDUINO/Carabine/Projet_Carabine.ino
+++ ARDUINO/Carabine/Projet_Carabine.ino
@@ -1,105 +1,162 @@
-//#include <avr/sleep.h>
+#include "Arduino.h"
+#include "string.h" 
+#include <LiquidCrystal.h>  /*on importe la bibliothèque pour le LCD*/
 
-int volatile Tirs, pinGachette, pinRecharge;
-boolean volatile Cote, Centre, ATire;
+LiquidCrystal lcd(12, 11, 10, 9, 6, 5); /* LiquidCrystal nomDeVariable(RS, E, d4, d5, d6, d7); */
+void WelcomeUser();
 
-/* ----- Attention ceci est un test ---- 
-int wakePin = 2;                 // pin used for waking up
-int sleepStatus = 0;             // variable to store a request for sleep
-int count = 0;                   // counter
------ Fin du test ----- */
+int volatile Tirs, pinGachette, pinRecharge, User, pinUser;
+boolean volatile ATire;
+int Res_Tir;
+
+
 
 
 void setup() 
 { 
+  lcd.begin(16, 2);
   Serial.begin(9600);
-  
-/* ----- Attention ceci est un test ---- 
-  pinMode(wakePin, INPUT);
- ----- Fin du test ----- */
-  
-
-  pinGachette=7; //Interrupteurs Gachette et Recharge sur ports 7 et 6
-  pinRecharge=1;
+  pinGachette=7; /* Interrupteurs Gachette et Recharge sur ports 7 et 6 */
+  pinRecharge=2;
+  pinUser=3;
   Tirs=0;       
-  Cote=false;
-  Centre=false;
   ATire=false;
-  pinMode(8,OUTPUT);  //Trigger du laser en mode sortie
-  digitalWrite(8, LOW); //trigger à 0
-  attachInterrupt(digitalPinToInterrupt(pinGachette),RoutineDeTir,FALLING); //Gachette et recharge déclanchées à l'appui dse bouttons
+  User=1;
+  Res_Tir=0;
+  pinMode(8,OUTPUT);  /* Trigger du laser en mode sortie */
+  digitalWrite(8, LOW); /* trigger à 0 */
+  attachInterrupt(digitalPinToInterrupt(pinGachette),RoutineDeTir,FALLING); /* Gachette, recharge et changement d'utilisateur déclanchés à l'appui dse bouttons */
   attachInterrupt(digitalPinToInterrupt(pinRecharge),RoutineRecharge,FALLING);
-
-/* ----- Attention ceci est un test ----   
-  attachInterrupt(0, sleepNow, LOW); // use interrupt 0 (pin 2) and run function
-                                      // wakeUpNow when pin 2 gets LOW
- ----- Fin du test ----- */
-
+  attachInterrupt(digitalPinToInterrupt(pinUser),ChangeUser,FALLING);
+  Serial.println(User);
+  WelcomeUser();
 }
+
 
 void loop() 
 {
-  while(ATire!=true && Tirs!=5){}; //On attend un tir
-  delay(75);  //on laisse allumé 75ms
-  digitalWrite(8,LOW); //On éteint le laser pour éviter de rester appuyé et tricher, faut pas déconner non plus.
-  ATire=false;
-}
-
-void RoutineRecharge() //Routine appelée à l'appui du bouton de recharge
-{
-  if(Tirs==5)   //Si on a bien effectué nos 5 tirs, on réinitialise à 0 tirs pour recommencer
-  {
-    Serial.println("Recharge effectuee");
-   Tirs=0;
-  }
-  else        //Sinon on attend que les 5 tirs soient réalisés
-  {
-    Serial.println("Vous n'avez pas effectue tous vos tirs...");
-  }
-}
-
-void RoutineDeTir() //Routine appelée à l'appui de la "gachette"
-{
-  if(Tirs<5) //Si on a encore des munitions on comptabilise un tir et on tire avec le laser (trigger port 8)
+  while(ATire!=true && Tirs<6){ }; /* On attend un tir */
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(0, 0);
+  
+  if(Tirs<5) /* Si on a encore des munitions, on tire avec le laser (trigger port 8) */
   {
     Tirs++;
-  digitalWrite(8,HIGH);
-  Serial.print("Vous venez de tirer ");  //On affiche le nombre de tirs effectués, histoire de pas devoir compter dans sa tête.
-  Serial.print(Tirs);
-  Serial.println(" fois "); 
+    digitalWrite(8,HIGH);
+    delay(100);  /* on laisse allumé 100ms. */
+    digitalWrite(8,LOW); /* On éteint le laser pour éviter de rester appuyé et tricher, faut pas déconner non plus.*/
+    lcd.print(User);  /* On affiche le num d'utilisateur et le numéro du tir effectué */
+    lcd.setCursor(1, 0);
+    lcd.print(") ");
+    lcd.setCursor(3, 0);
+    lcd.print("Tir ");
+    lcd.setCursor(9, 0);
+    lcd.print(Tirs);
+    if(Serial.available() > 0) /* On récupère la valeur du tir et on affiche le résultat */
+    {
+      Res_Tir=Serial.read(); 
+    }
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    if(Res_Tir>0)
+    {
+      lcd.print("Touche a");
+      lcd.setCursor(0, 9);
+      lcd.print(Res_Tir);
+      lcd.setCursor(12, 0);    
+      lcd.print("%");
+    }
+    else
+    {
+      lcd.print("Echec!");    
+    }
+    Res_Tir=0;
   }
   else
   {
-    Serial.println("Vous devez recharger"); // Si on a tiré 5 fois on attend une recharge pour pouvoir continuer
+    lcd.print(User);  /* On affiche le nombre de tirs effectués, histoire de pas devoir compter dans sa tête. */
+    lcd.setCursor(1, 0);
+    lcd.print(") ");
+    lcd.setCursor(3, 0);
+    lcd.print("Rechargez");
+    lcd.setCursor(0,1);
+    lcd.print("                ");
   }
-  ATire=true; // On signale qu'on a tiré pour déclancher la suite du programme
+  
+  delay(300); /* On attend un peu pour laisser le temps à la cible de faire l'acquisition */
+  ATire=false; /* On se remet en attente d'un tir. */
+  Serial.println(Tirs);
 }
 
 
-/* ----- Attention ceci est un test ---- 
 
-void sleepNow()         // here we put the arduino to sleep
-{    
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
-  sleep_enable();          // enables the sleep bit in the mcucr register
-                             // so sleep is possible. just a safety pin
 
-  attachInterrupt(0,wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
-                                       // wakeUpNow when pin 2 gets LOW
- 
-  sleep_mode();            // here the device is actually put to sleep!!
-                             // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
- 
-  sleep_disable();         // first thing after waking from sleep:
-                             // disable sleep...
-  detachInterrupt(0);      // disables interrupt 0 on pin 2 so the
-                             // wakeUpNow code will not be executed
-                             // during normal running time.
-}
 
-void wakeUpNow()
+
+void RoutineRecharge() /* Routine appelée à l'appui du bouton de recharge */
 {
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(0, 0);
+  if(Tirs==5)   /* Si on a bien effectué nos 5 tirs, on réinitialise à 0 tirs pour recommencer */
+  {
+    lcd.print("Recharge faite");
+    Tirs=0;
+  }
+  else        /*Sinon on attend que les 5 tirs soient réalisés */
+  {
+    lcd.print("balles restantes");
+  }
+  lcd.setCursor(0,1);
+  lcd.print("Tir possible !");
 }
 
- ----- Fin du test ----- */
 
+
+
+
+
+void RoutineDeTir() /*Routine appelée à l'appui de la "gachette" */
+{
+  ATire=true; /* On signale qu'on a tiré pour déclancher la suite du programme */
+}
+
+
+
+
+
+void ChangeUser() /* A l'appui du boutton on change d'utilisateur (9 maximum) et on envoie le nouvel utilisateur à la cible et à l'écran */
+{
+  if(User<9)
+  {
+    User++;  
+  }
+  else
+  {
+    User=1;
+  }
+  Serial.println(User);
+  WelcomeUser();
+  Tirs=0;
+}
+
+
+
+
+
+void WelcomeUser()
+{
+  lcd.setCursor(0,0);
+  lcd.print("                ");
+  lcd.setCursor(0,0);
+  lcd.print("Welcome user ");
+  lcd.setCursor(13, 0);
+  lcd.print(User);
+  lcd.setCursor(0,1);
+  lcd.print("                ");
+  lcd.setCursor(0,1);
+  lcd.print("Tir possible!");
+  delay(300);
+}
